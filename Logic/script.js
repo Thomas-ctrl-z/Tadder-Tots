e9f2e3099139976b6c5235f919d514b0a203427b
Jaeden Souden <jaedensouden@gmail.com>
2026-05-14 21:43:04 -0500

    Added Sidebar and styling. Stable

--- Logic/script.js
+++ Logic/script.js
@@ -1,15 +1,17 @@
-const openCartBtn = document.getElementById('open-cart-button');
-const closeCartBtn = dpcument.getElementById('close-cart-button');
+const openCartBtn = document.getElementById("open-cart-btn");
+const closeCartBtn = document.getElementById("close-cart-btn");
 
-const cartSideBar = document.getElementById('cart-sidebar');
-const cartOverlay = document.getElementById('cart-overlay');
+const cartSideBar = document.getElementById("cart-sidebar");
+const cartOverlay = document.getElementById("cart-overlay");
 
 openCartBtn.addEventListener("click", () => {
     cartSideBar.classList.add("active");
     cartOverlay.classList.add("active");
+    document.body.classList.add("no-scroll");
 });
 
 closeCartBtn.addEventListener("click", () => {
     cartSideBar.classList.remove("active");
     cartOverlay.classList.remove("active");
+    document.body.classList.remove("no-scroll");
 });
